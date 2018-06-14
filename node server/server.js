97e960f930ae9aee473ed354a5a4e6e129bd188e
Toms <toms63@yahoo.com>
2018-06-14 18:08:13 +0200

    updated node server.

--- node server/server.js
+++ node server/server.js
@@ -7,14 +7,14 @@ app.get('/', function(req, res){
 });
 io.on('connection', function(socket){
   console.log('user connected');
-  socket.on('chat', function(msg){
-      console.log(msg)
-    io.emit('render', msg);
+  socket.on('my_event', function(msg){
+    console.log(msg)
+    io.emit('my_response', {'data': msg['data'], 'senderid': msg['senderid'] ,'len': msg['len'] });
   });
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
 });
-http.listen(4000, function(){
-  console.log('listening on *:4000');
+http.listen(5000, function(){
+  console.log('listening on *:5000');
 });
